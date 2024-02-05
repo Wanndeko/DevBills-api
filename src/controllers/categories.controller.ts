6e93362ef8cc96e9f136a6a3a54c8137a7e18957
wanderley <camposwanderley418@gmail.com>
2024-02-05 20:56:57 -0300

    adicionando repositorie Pattern, Dtos e cat de errors

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
 import { CategoriesService } from "../services/categories.service";
+import { CategoriesRepository } from "../database/repositories/categories.repository";
+import { CategoryModel } from "../database/schemas/category.schema";
+import { CreateCategoryDTO } from "../dtos/categories.dto";
 
 export class CategoriesController{
-    async create(_: Request, response: Response){
-        const service = new CategoriesService()
-        const result = await service.create()
+    async create(
+        request: Request<unknown, unknown, CreateCategoryDTO>,
+        response: Response){
+
+        const {title, color} = request.body
+        
+        const repository  =  new CategoriesRepository(CategoryModel)
+        const service = new CategoriesService(repository)
+        const result = await service.create({title, color})
         return response.status(201).json(result)
     }
 }
--- src/services/categories.service.ts
+++ src/services/categories.service.ts
@@ -1,11 +1,24 @@
+import { CategoriesRepository } from "../database/repositories/categories.repository";
+import { CreateCategoryDTO } from "../dtos/categories.dto";
 import { Category } from "../entities/category.entity";
 
 export class CategoriesService {
-    async create(): Promise<Category>{
+    constructor(private categoriesRepository : CategoriesRepository){}
+
+    async create({title,color}: CreateCategoryDTO): Promise<Category>{
+        const foundCategory = await this.categoriesRepository.findByTitle(title)
+
+        if(foundCategory){
+            throw new Error('Category already exists.')
+        }
+
         const category = new Category({
-            title: "example category",
-            color: "#ff33bb"
+            title,
+            color 
         })
-        return category
+
+        const createdCategory = await this.categoriesRepository.create(category)
+
+        return createdCategory
     }
 }
